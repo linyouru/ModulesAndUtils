ce3d03a1fa0e00f6387db48859132fdef0fe489d
lyr <270809304@qq.com>
2019-08-09 10:29:29 +0800

    修改websocket配置，实现后端根据sid将消息推送给对应的用户

---
+++ WebSocket/src/main/java/lyr/websocket/controller/WebSocketController.java
@@ -0,0 +1,35 @@
+package lyr.websocket.controller;
+
+import lyr.websocket.service.WebSocketServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+
+/**
+ * @ClassName WebSocketController
+ * @Description TODO
+ * @Author LYR
+ * @Date 2019/8/7 14:51
+ * @Version 1.0
+ **/
+@RestController
+public class WebSocketController {
+
+    @Resource
+    private WebSocketServer webSocket;
+
+    @RequestMapping("/getMsg")
+    public void getMsg(){
+
+        try {
+            webSocket.sendMessageBySid("123123123","001");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
--- WebSocket/src/main/java/lyr/websocket/service/WebSocketServer.java
+++ WebSocket/src/main/java/lyr/websocket/service/WebSocketServer.java
@@ -1,11 +1,14 @@
 package lyr.websocket.service;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.server.standard.SpringConfigurator;
 
 import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
@@ -16,7 +19,7 @@ import java.util.concurrent.CopyOnWriteArraySet;
  * @Version 1.0
  **/
 
-@ServerEndpoint("/websocket")
+@ServerEndpoint(value = "/websocket/{sid}")
 @Component
 public class WebSocketServer {
 
@@ -24,8 +27,8 @@ public class WebSocketServer {
     private static int onlineCount = 0;
 
     //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。
-    private static CopyOnWriteArraySet<WebSocketServer> webSocketSet = new CopyOnWriteArraySet<WebSocketServer>();
-
+//    private static CopyOnWriteArraySet<WebSocketServer> webSocketSet = new CopyOnWriteArraySet<WebSocketServer>();
+    private static Map<String,CopyOnWriteArraySet<Session>> webSocketMap = new HashMap<>();
     //与某个客户端的连接会话，需要通过它来给客户端发送数据
     private Session session;
 
@@ -36,25 +39,19 @@ public class WebSocketServer {
     @OnOpen
     public void onOpen(Session session,@PathParam("sid") String sid) {
         this.session = session;
-        webSocketSet.add(this);     //加入set中
         addOnlineCount();           //在线数加1
         System.out.println("有新窗口开始监听:"+sid+",当前在线人数为" + getOnlineCount());
         this.sid=sid;
-        try {
-            sendMessage("连接成功");
-        } catch (IOException e) {
-            System.out.println("websocket IO异常");
-        }
+        addSession(session,sid);
     }
 
     /**
      * 连接关闭调用的方法
      */
     @OnClose
-    public void onClose() {
-        webSocketSet.remove(this);  //从set中删除
-        subOnlineCount();           //在线数减1
-        System.out.println("有一连接关闭！当前在线人数为" + getOnlineCount());
+    public void onClose(Session session, @PathParam("sid") String sid) {
+        this.session = session;
+        removeSession(session,sid);
     }
 
     /**
@@ -62,15 +59,13 @@ public class WebSocketServer {
      *
      * @param message 客户端发送过来的消息*/
     @OnMessage
-    public void onMessage(String message, Session session) {
+    public void onMessage(String message, Session session, @PathParam("sid") String sid) {
         System.out.println("收到来自窗口"+sid+"的信息:"+message);
-        //群发消息
-        for (WebSocketServer item : webSocketSet) {
-            try {
-                item.sendMessage(message);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        this.session = session;
+        try {
+            sendMessageBySid(message,sid);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -80,47 +75,65 @@ public class WebSocketServer {
      * @param error
      */
     @OnError
-    public void onError(Session session, Throwable error) {
-        System.out.println("发生错误");
+    public void onError(Session session, Throwable error, @PathParam("sid") String sid) {
+        this.session =session;
+        removeSession(session, sid);
         error.printStackTrace();
     }
     /**
-     * 实现服务器主动推送
+     * 推送消息到对应的sid
      */
-    public void sendMessage(String message) throws IOException {
-        this.session.getBasicRemote().sendText(message);
+    public void sendMessageBySid(String message,String sid) throws IOException {
+        CopyOnWriteArraySet<Session> array = webSocketMap.get(sid);
+        if(array !=null && array.size()>0){
+            for (Session item : array) {
+                item.getBasicRemote().sendText(message);
+            }
+        }
     }
 
 
     /**
      * 群发自定义消息
      * */
-    public static void sendInfo(String message,@PathParam("sid") String sid) throws IOException {
-        System.out.println("推送消息到窗口"+sid+"，推送内容:"+message);
-        for (WebSocketServer item : webSocketSet) {
-            try {
-                //这里可以设定只推送给这个sid的，为null则全部推送
-                if(sid==null) {
-                    item.sendMessage(message);
-                }else if(item.sid.equals(sid)){
-                    item.sendMessage(message);
-                }
-            } catch (IOException e) {
-                continue;
+    public void sendAllMessage(String message) throws IOException {
+        if(webSocketMap.size()>0){
+            for (String sid : webSocketMap.keySet()) {
+                sendMessageBySid(message,sid);
             }
         }
     }
 
-    public static synchronized int getOnlineCount() {
+    private static synchronized int getOnlineCount() {
         return onlineCount;
     }
 
-    public static synchronized void addOnlineCount() {
+    private static synchronized void addOnlineCount() {
         WebSocketServer.onlineCount++;
     }
 
-    public static synchronized void subOnlineCount() {
+    private static synchronized void subOnlineCount() {
         WebSocketServer.onlineCount--;
     }
 
+    private static synchronized void addSession(Session session, String sid) {
+        CopyOnWriteArraySet<Session> array = webSocketMap.get(sid);
+        if (array == null || array.size() == 0) {
+            array = new CopyOnWriteArraySet<>();
+        }
+        array.add(session);
+        webSocketMap.put(sid, array);
+    }
+
+    private static synchronized void removeSession(Session session, String sid) {
+        if (webSocketMap.size() > 0) {
+            CopyOnWriteArraySet<Session> array = webSocketMap.get(sid);
+            if (array != null && array.size() > 0) {
+                array.remove(session);
+//                webSocketMap.put(sid, array);
+            }
+        }
+        subOnlineCount();           //在线数减1
+        System.out.println(""+sid+"连接关闭！当前在线人数为" + getOnlineCount());
+    }
 }
