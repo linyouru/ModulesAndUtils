095626a5c6afcdd292090245d9d3f2bbe2f0c33b
lijunru <22251718@qq.com>
2019-08-29 15:17:05 +0800

    增加EasyExcel工具及使用例子

--- Utils/src/main/java/lyr/utils/controller/ExportExcel.java
+++ Utils/src/main/java/lyr/utils/controller/ExportExcel.java
@@ -1,11 +1,14 @@
 package lyr.utils.controller;
 
+import lyr.utils.utils.EasyExcelUtils;
 import lyr.utils.utils.ExcelReaderUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @ClassName test
@@ -25,4 +28,29 @@ public class ExportExcel {
         System.out.println(123);
 
     }
+
+    @RequestMapping("/EasyExcelTest.do")
+    public void EasyExcelTest(){
+        //EasyExcelTestLittleDemo
+
+        //导出
+        String filepath = "C:\\Users\\asus\\Desktop\\EasyExcelTest.xlsx";
+        File file = new File("C:\\Users\\asus\\Desktop\\EasyExcelTest.xlsx");
+        List<List<String>> dataList = new ArrayList<>();
+        List<String> list1 = new ArrayList<>();
+        List<String> list2 = new ArrayList<>();
+        String sheetName = "testsheet1";
+        list1.add("11111111111111111");
+        list1.add("22222222222222222");
+        list2.add("33333333333333333");
+        list2.add("44444444444444444");
+        dataList.add(list1);
+        dataList.add(list2);
+        EasyExcelUtils.writeExcelByString(filepath,sheetName,dataList);
+
+        //读取
+        Map<String, List<List<String>>> map =  EasyExcelUtils.readExcelByString(filepath);
+        System.out.println(map);
+    }
+
 }
