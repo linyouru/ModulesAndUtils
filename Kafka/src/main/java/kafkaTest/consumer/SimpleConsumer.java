68eee24eb07221c1643e489eb6346bf85fa756f2
lijunru <22251718@qq.com>
2019-10-11 17:02:10 +0800

    新增模块 java+Kafka

---
+++ Kafka/src/main/java/kafkaTest/consumer/SimpleConsumer.java
@@ -0,0 +1,20 @@
+package kafkaTest.consumer;
+
+
+import com.google.gson.Gson;
+import kafkaTest.common.MessageEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class SimpleConsumer {
+
+    private final Gson gson = new Gson();
+
+    @KafkaListener(topics = "${kafka.topic.default}", containerFactory = "kafkaListenerContainerFactory")
+    public void receive(MessageEntity message) {
+        log.info(gson.toJson(message));
+    }
+}
