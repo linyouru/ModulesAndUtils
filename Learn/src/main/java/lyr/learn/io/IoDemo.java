47155c2758ddb23584c44f3e5f0ea0e8cd1c1ccf
linyouru1 <linyouru@zlg.cn>
2023-01-13 14:42:52 +0800

    io

--- Learn/src/main/java/lyr/learn/io/IoDemo.java
+++ Learn/src/main/java/lyr/learn/io/IoDemo.java
@@ -188,6 +188,8 @@ public class IoDemo {
 }
 
 class User1 implements Serializable {
+
+    private static final long serialVersionUID = 1L;
     private String name;
     private int age;
 
--- Learn/src/main/java/lyr/learn/thread/MyCallable.java
+++ Learn/src/main/java/lyr/learn/thread/MyCallable.java
@@ -4,6 +4,7 @@ import java.util.concurrent.*;
 
 /**
  * Callable实现多线程
+ * 自从Java 1.5开始，提供了Callable和Future，通过它们可以在任务执行完毕之后得到任务执行结果。
  * @ClassName MyCallable
  * @Description TODO
  * @Author LYR
@@ -15,25 +16,29 @@ public class MyCallable {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
         //创建连接池
-        ExecutorService pool = Executors.newFixedThreadPool(10);
+        //不建议使用Executors创建线程池,有oom风险
+//        ExecutorService pool = Executors.newFixedThreadPool(10);
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(10, 10, 60L, TimeUnit.SECONDS, new ArrayBlockingQueue<>(10));
         for (int i=0;i<10;i++){
-            Callable<Integer> callable1 = new Callable1();
+            Callable<Double> callable1 = new Callable1();
             FutureTask futureTask =new FutureTask(callable1);
-//            new Thread(futureTask).start(); //不建议显示使用线程
-            pool.submit(futureTask);
+            new Thread(futureTask).start(); //不建议显示使用线程
+//            pool.submit(futureTask);
+            threadPoolExecutor.submit(futureTask);
             System.out.println(Thread.currentThread().getName()+"--------"+futureTask.get());
         }
-        pool.shutdown();
+//        pool.shutdown();
+        threadPoolExecutor.shutdown();
     }
 
 }
 
-class Callable1 implements Callable<Integer>{
+class Callable1 implements Callable<Double>{
 
     @Override
-    public Integer call() throws Exception {
+    public Double call() throws Exception {
 
         System.out.println(Thread.currentThread().getName());
-        return 0;
+        return Math.random();
     }
 }
